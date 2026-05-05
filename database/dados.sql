ba74419fd89e124d9ea59fb971dd7dee4eafd533
Júlio César <juliocbizarria@gmail.com>
2026-05-05 17:34:15 -0300

    insert em cargos e departamentos

---
+++ database/dados.sql
@@ -0,0 +1,14 @@
+USE cidigital;
+
+INSERT INTO cargos(id,nome) VALUES
+(1,'administrador_do_sistema'),
+(2,'gerente_administrativo'),
+(3,'supervisor'),
+(4,'auxiliar_administrativo');
+
+
+INSERT INTO departamento(id,nome,descricao) VALUES
+(1,'DP','departamento_pessoal'),
+(2,'GA','gerencia_administrativa'),
+(3,'SST','seguranca_do_trabalho'),
+(4,'RH','recursos_humanos');
