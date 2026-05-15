b45f69e3915a9526ec619bb47e09892e68b4cad4
Júlio César <juliocbizarria@gmail.com>
2026-05-14 22:02:08 -0300

    testes de busca e listagem de usuario implementados

--- backend/tests/usuario.test.js
+++ backend/tests/usuario.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 
 const BASE_URL = 'http://localhost:3000';
 
+//teste para verificar se servidor está respondendo
 describe('Servidor', () => {
   test('deve estar online', async () => {
     const res = await request(BASE_URL).get('/api/health');
@@ -9,6 +10,8 @@ describe('Servidor', () => {
     expect(res.body.sucesso).toBe(true);
   });
 });
+
+/*teste para cadastro de usuarios
 
 describe('Usuarios', () => {
   test('deve cadastrar um usuario', async () => {
@@ -22,7 +25,9 @@ describe('Usuarios', () => {
     expect(res.status).toBe(201);
     expect(res.body.sucesso).toBe(true);
   });
+*/
 
+//teste para login
   test('deve realizar login', async () => {
     const res = await request(BASE_URL).post('/api/usuarios/login').send({
       email: 'teste2@cidigital.com',
@@ -33,6 +38,7 @@ describe('Usuarios', () => {
     expect(res.body.dados.token).toBeDefined();
   });
 
+//teste para login com senha errada
   test('deve rejeitar login com senha errada', async () => {
     const res = await request(BASE_URL).post('/api/usuarios/login').send({
       email: 'teste2@cidigital.com',
@@ -41,4 +47,27 @@ describe('Usuarios', () => {
     expect(res.status).toBe(401);
     expect(res.body.sucesso).toBe(false);
   });
+
+//tenta listar os usuarios sem enviar token
+test('rejeitar acesso sem token', async () => {
+    const res = await request(BASE_URL).get('/api/usuarios');
+    expect(res.status).toBe(401);
+    expect(res.body.sucesso).toBe(false);
+});
+
+//teste para buscar usuario pelo id com token
+
+test('deve buscar usuario com token', async () => {
+  const loginRes = await request(BASE_URL).post('/api/usuarios/login').send({
+    email: 'teste2@cidigital.com',
+    senha: 'Senha@123'
+  });
+  const token = loginRes.body.dados.token;
+
+  const res = await request(BASE_URL)
+    .get(`/api/usuarios/${loginRes.body.dados.usuario.id}`)
+    .set('Authorization', `Bearer ${token}`);
+
+  expect(res.status).toBe(200);
+  expect(res.body.sucesso).toBe(true);
 });
